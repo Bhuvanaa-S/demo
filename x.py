4f55f1ad23f0028765aac270721e1475aefae0c4
Bhuvanaa-S <bhuvanaaa2006@gmail.com>
2025-05-13 09:33:19 +0530

    git commit

--- x.py
+++ x.py
@@ -1,4 +1,14 @@
-a = 2
-b = 3
-c = a+b
-print(c)
+import unittest
+
+# Function to test
+def add(x, y):
+    return x + y
+
+# Test case class
+class SimpleTest(unittest.TestCase):
+    def test_add1(self):
+        self.assertEqual(add(4, 5), 9)
+
+# Run tests
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
